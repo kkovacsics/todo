4665a58986e0488f56109922802ae1a41182de6c
kkovacsics <kalman.kovacsics@gmail.com>
2020-12-11 21:06:47 +0100

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,57 +1,96 @@
 'use strict';
 
-const now = new Date();
-const date = document.querySelector('.todo__date')
-date.innerHTML = `${now.toLocaleDateString('en-US', {weekday: 'long'})}
-                    <br>
-                    ${now.toLocaleDateString('en-US', {year: 'numeric', month: '2-digit', day: '2-digit'}).replace(/\//g,'-')}`;
+// used elements
+const todo = document.querySelector('.todo');
 
-const todo_pending = document.querySelector('.todo__pending');
+const todo_lists = todo.querySelector('.todo__lists');
+const todo_nothing = todo.querySelector('.todo__nothing');
+
+// pending
+const todo_pending = todo.querySelector('.todo__pending');
 const todo_pending_list = todo_pending.querySelector('.todo__list');
+const pendingInfo = todo_pending.querySelector('.todo__info span');
+// completed
 const todo_completed = document.querySelector('.todo__completed');
 const todo_completed_list = todo_completed.querySelector('.todo__list');
-const pendingInfo = todo_pending.querySelector('.todo__info');
-const completedInfo = todo_completed.querySelector('.todo__info');
+const completedInfo = todo_completed.querySelector('.todo__info span');
 
-document.querySelector('.todo__handler span:first-child').addEventListener('click', function(){
+// handling localStorage
+const localDB = {
+    getTodosFromLocalStorage(){
+        if(localStorage.todos){         // there is saved todos list
+            JSON.parse(localStorage.todos)
+                .forEach(item => insertTodoItem(item, false));
+        }
+    },
+    saveTodosToLocalStorage(){
+        const todos = [];
+        todo_pending_list.querySelectorAll('.todo__item')
+                         .forEach(item => todos.unshift(item.querySelector('span').textContent));
+        localStorage.todos = JSON.stringify(todos);
+    },
+}
+// refresh lists' info - You have pending items; Completed tasks;
+const doRefresh = {
+    pending(){
+        const pending_items = todo_pending_list.querySelectorAll('.todo__item');
+        pendingInfo.textContent = pending_items.length;
+    },
+    completed(){
+        const completed_items = todo_completed_list.querySelectorAll('.todo__item');
+        const pending_items = todo_pending_list.querySelectorAll('.todo__item');
+        let ratio = (completed_items.length/(pending_items.length+completed_items.length)*100).toFixed(0);
+        completedInfo.textContent = `${ratio==='NaN'? '': ratio}`;
+    },
+}
+function refreshPage(){
+    doRefresh.pending();                // pending info
+    doRefresh.completed();              // completed info
+    localDB.saveTodosToLocalStorage();  // save pending todos
+
+    // pendingInfo.textContent => number of pending todo
+    if(pendingInfo.textContent==='0'){  // no pending
+        setTimeout(() => {              // not immediately :)
+            todo_lists.classList.remove('show');
+            todo_nothing.classList.add('show');
+        }, 500);
+    } else {
+        todo_lists.classList.add('show');
+        todo_nothing.classList.remove('show')
+    }
+};
+
+// read todos list
+localDB.getTodosFromLocalStorage();
+// display stat info
+refreshPage();
+
+// actual date
+const now = new Date();
+todo.querySelector('.todo__date')
+    .innerHTML = `${now.toLocaleDateString('en-US', {weekday: 'long'})}
+        <br>
+        ${now.toLocaleDateString('en-US', {year: 'numeric', month: '2-digit', day: '2-digit'}).replace(/\//g,'-')}`;
+
+// Hide/Show, button's eventhandlers
+todo.querySelector('.todo__handler span:first-child').addEventListener('click', function(){
     todo_completed.classList.toggle('show');
     this.textContent = todo_completed.classList.contains('show')? 'Hide Completed': 'Show Completed';
 });
-
-document.querySelector('.todo__handler span:last-child').addEventListener('click', function(){
+// Clear button's eventhandlers
+todo.querySelector('.todo__handler span:last-child').addEventListener('click', function(){
     const pending_items = todo_pending_list.querySelectorAll('.todo__item');
     if(pending_items.length>0){     // there are items in the list
         pending_items.forEach(item => deleteTodoItem(item));
     }
 });
 
-(function getTodosFromLocalStorage(){
-    if(localStorage.todos){
-        const todos = JSON.parse(localStorage.todos);
-        todos.forEach(item => insertTodoItem(item));
-    }
-})();
-function saveTodosToLocalStorage(){
-    const pending_items = todo_pending_list.querySelectorAll('.todo__item');
-    const todos = [];
-    pending_items.forEach(item => todos.unshift(item.querySelector('span').textContent));
-    localStorage.todos = JSON.stringify(todos);
-};
-
-function refreshInfo(){
-    const pending_items = todo_pending_list.querySelectorAll('.todo__item');
-    pendingInfo.textContent = `You have ${pending_items.length} pending items`;
-    const completed_items = todo_completed_list.querySelectorAll('.todo__item');
-    let ratio = (completed_items.length/(pending_items.length+completed_items.length)*100).toFixed(0);
-    completedInfo.textContent = `Completed tasks: ${ratio==='NaN'? '': ratio}%`;
-    saveTodosToLocalStorage();
-};
-refreshInfo();
-
+// delete
 function deleteTodoItem(item){
     item.parentElement.removeChild(item);
-    refreshInfo();
+    refreshPage();
 };
+// complete
 function completeTodoItem(item){
     const completedItemHTML =   `<div class="todo__item">
                                     <span class="checked">&#10004;</span>
@@ -60,26 +99,31 @@ function completeTodoItem(item){
     todo_completed_list.insertAdjacentHTML('afterbegin', completedItemHTML);
     deleteTodoItem(item);                       
 }
-
-
-function insertTodoItem(todo_text){
+// insert new todo item into todo-list
+function insertTodoItem(todo_text, animation=true){
     if(todo_text.length){         // there is new todo
-        const todoItemHTML =    `<div class="todo__item">
+        const todoItemHTML =    `<div class="todo__item todo__item--right">
                                     <input type="checkbox">
                                     <span>${todo_text}</span>
                                     <button>&#10006;</button>
                                 </div>`;
-        todo_pending_list.insertAdjacentHTML('afterbegin', todoItemHTML);
-        todo_pending_list.querySelector('.todo__item button').addEventListener('click', function(){
+        todo_pending_list.insertAdjacentHTML('afterbegin', todoItemHTML);   // it will be the first child
+        const todo_item = todo_pending_list.querySelector('.todo__item:first-child');
+        if(animation){
+            setTimeout(() => todo_item.classList.remove('todo__item--right'), 0);
+        } else {
+            todo_item.classList.remove('todo__item--right');
+        }
+        todo_item.querySelector('button').addEventListener('click', function(){
             deleteTodoItem(this.parentElement);
         });
-        todo_pending_list.querySelector('.todo__item [type=checkbox]').addEventListener('click', function(){
+        todo_item.querySelector('[type=checkbox]').addEventListener('click', function(){
             completeTodoItem(this.parentElement);
         });
-        refreshInfo();
+        refreshPage();
     }
 };
-
+// handle new todo item
 document.querySelector('.todo__btn').addEventListener('click', function(){
     const todo_text = document.querySelector('.todo__text');
     insertTodoItem(todo_text.value.trim());
